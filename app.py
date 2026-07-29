5f282aec37f6abe23c163ac2c70ec6f420de391c
huynhlekhang12345-cloud <huynhlekhang12345@gmail.com>
2026-07-29 23:01:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -357,7 +357,7 @@ if __name__ == '__main__':
     import sys
     keep_alive()
 
-    TOKEN = "TOKEN_BOT_CỦA_BẠN"
+    TOKEN = "8948413828:AAFKfmqJA_By7L63kgOF87-4iwwArnU3vpk"
     
     app = ApplicationBuilder().token(TOKEN).build()
     
