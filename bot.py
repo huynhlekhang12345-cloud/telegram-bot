5e531c86f6807b819be57d293e704899ceea2ce9
huynhlekhang12345-cloud <huynhlekhang12345@gmail.com>
2026-07-30 11:11:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,7 @@ import telebot
 from flask import Flask
 
 # 1. Khởi tạo Bot Telegram
-TOKEN = "8948413828:AAEq76h9wvJPvV46W7lau-ENFcoJHoc5BgI
-"
+TOKEN = "8948413828:AAEq76h9wvJPvV46W7lau-ENFcoJHoc5BgI"
 bot = telebot.TeleBot(TOKEN)
 
 # Định nghĩa các lệnh cho bot của bạn ở đây
